157a1c96b0bdf94c081a66952ff73bb37c1cbd4d
kowsalya <kowsalya@twilightsoftwares.com>
2023-09-04 17:38:28 +0530

    push without vendor file

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    // return $router->app->version();
+    echo "JWT with lumen";
+});
+
+$router->post('/login', 'ExampleController@postLogin');
