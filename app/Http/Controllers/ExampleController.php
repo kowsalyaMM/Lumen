61c91357d2c42a11155ecdf86893ac26319fa9c9
kowsalya <kowsalya@twilightsoftwares.com>
2023-09-04 19:07:41 +0530

    example push

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -21,28 +21,37 @@ class ExampleController extends Controller
 
     public function postLogin(Request $request)
     {
+        
         $this->validate($request, [
             'email'    => 'required|email|max:255',
             'password' => 'required',
         ]);
 
-        try {
+        try 
+        {
 
-            if (! $token = $this->jwt->attempt($request->only('email', 'password'))) {
+            if (! $token = $this->jwt->attempt($request->only('email', 'password'))) 
+            {
                 return response()->json(['user_not_found'], 404);
             }
 
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        } 
+        catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) 
+        {
 
             return response()->json(['token_expired'], 500);
 
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        } 
+        catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) 
+        {
 
             return response()->json(['token_invalid'], 500);
 
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+        } 
+        catch (\Tymon\JWTAuth\Exceptions\JWTException $e) 
+        {
 
-            return response()->json(['token_absent' => $e->getMessage()], 500);
+            return response()->json(['token_absents' => $e->getMessage()], 500);
 
         }
 
